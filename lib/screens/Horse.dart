8dcf49eaadf5a42dfc1e8dc72770fd0795e5d353
Rui Gaspar <ruigaspar03@hotmail.com>
2022-10-30 00:44:57 +0200

    Update Horse.dart

--- lib/screens/Horse.dart
+++ lib/screens/Horse.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-
 import '../class/HorseModel.dart';
 import '../config/mongodb.dart';
+import 'package:mongo_dart/mongo_dart.dart' as M;
 import 'Nav.dart';
 
 class Horse extends StatefulWidget {
@@ -21,12 +21,20 @@ class _HorseState extends State<Horse> {
   List<TextEditingController> controllers =
       List<TextEditingController>.generate(8, (i) => TextEditingController());
 
-  late HorseModel horse = HorseModel('', '', '', '', '', '', '');
+  // late HorseModel horse = HorseModel(M.ObjectId(), '', '', '', '', '', '', '');
+
+  late String name;
+  late String photo;
+  late String age;
+  late String dress;
+  late String race;
+  late String gender;
+  late String speciality;
 
   @override
   void initState() {
     super.initState();
-    getHorses();
+    // getHorses();
   }
 
   @override
@@ -37,168 +45,289 @@ class _HorseState extends State<Horse> {
           title: const Text('Chevaux'),
         ),
         body: SafeArea(
-            child: Center(
-          child: Column(
-            children: [
-              const Text(
-                'Liste de vos chevaux',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              DropdownButton(
-                value: horse.name,
-                icon: const Icon(Icons.keyboard_arrow_down),
-                items: _horsesNames.map((String horse) {
-                  return DropdownMenuItem(
-                    value: horse,
-                    child: Text(horse),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  for (HorseModel horse in _horses) {
-                    if (horse.name == newValue) {
-                      horse = horse;
-                    }
-                  }
+            child: FutureBuilder(
+          future: MongoDatabase.getHorses(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              if (snapshot.hasData) {
+                var data = snapshot.data;
+                return SingleChildScrollView(
+                    child: Column(children: [
+                  for (var i = 0; i < data.length; i++)
+                    Card(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Text(data[i]["name"]),
+                            Text(data[i]["photo"]),
+                            Text(data[i]["age"]),
+                            Text(data[i]["dress"]),
+                            Text(data[i]["race"]),
+                            Text(data[i]["gender"]),
+                            Text(data[i]["speciality"]),
+                          ],
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.edit_note),
+                          color: Colors.black,
+                          onPressed: () => {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Basic dialog title'),
+                                    content: SingleChildScrollView(
+                                      child: ListBody(children: [
+                                        Column(
+                                          children: [
+                                            TextFormField(
+                                              onChanged: (value) {
+                                                data[i]["name"] = value;
+                                              },
+                                              initialValue: data[i]["name"],
+                                              decoration: const InputDecoration(
+                                                  labelText: 'Nom'),
+                                            ),
+                                            TextFormField(
+                                              onChanged: (value) {
+                                                data[i]["photo"] = value;
+                                              },
+                                              initialValue: data[i]["photo"],
+                                              decoration: const InputDecoration(
+                                                  labelText: 'Photo'),
+                                            ),
+                                            TextFormField(
+                                              onChanged: (value) {
+                                                data[i]["age"] = value;
+                                              },
+                                              initialValue: data[i]["age"],
+                                              decoration: const InputDecoration(
+                                                  labelText: 'Age'),
+                                            ),
+                                            TextFormField(
+                                              onChanged: (value) {
+                                                data[i]["dress"] = value;
+                                              },
+                                              initialValue: data[i]["dress"],
+                                              decoration: const InputDecoration(
+                                                  labelText: 'Robe'),
+                                            ),
+                                            TextFormField(
+                                              onChanged: (value) {
+                                                data[i]["race"] = value;
+                                              },
+                                              initialValue: data[i]["race"],
+                                              decoration: const InputDecoration(
+                                                  labelText: "Race"),
+                                            ),
+                                            TextFormField(
+                                              onChanged: (value) {
+                                                data[i]["gender"] = value;
+                                              },
+                                              initialValue: data[i]["gender"],
+                                              decoration: const InputDecoration(
+                                                  labelText: "Gender"),
+                                            ),
+                                            TextFormField(
+                                              onChanged: (value) {
+                                                data[i]["speciality"] = value;
+                                              },
+                                              initialValue: data[i]
+                                                  ["speciality"],
+                                              decoration: const InputDecoration(
+                                                  labelText: "Speciality"),
+                                            ),
+                                          ],
+                                        )
+                                      ]),
+                                    ),
+                                    actions: <Widget>[
+                                      TextButton(
+                                          style: TextButton.styleFrom(
+                                            textStyle: Theme.of(context)
+                                                .textTheme
+                                                .labelLarge,
+                                          ),
+                                          child: const Text('Cancel'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          }),
+                                      TextButton(
+                                        style: TextButton.styleFrom(
+                                          textStyle: Theme.of(context)
+                                              .textTheme
+                                              .labelLarge,
+                                        ),
+                                        child: const Text('Confirm'),
+                                        onPressed: () async {
+                                          await MongoDatabase.updateHorse(
+                                              data[i]["_id"],
+                                              data[i]["name"],
+                                              data[i]["photo"],
+                                              data[i]["age"],
+                                              data[i]["dress"],
+                                              data[i]["race"],
+                                              data[i]["gender"],
+                                              data[i]["speciality"]).whenComplete(() => Navigator.of(context).pop());
+                                          setState(() {
 
-                  horse.name = newValue!;
-
-                  controllers[0].text = horse.name!;
-                  controllers[1].text = horse.photo!;
-                  controllers[2].text = horse.age!;
-                  controllers[3].text = horse.dress!;
-                  controllers[4].text = horse.race!;
-                  controllers[5].text = horse.gender!;
-                  controllers[6].text = horse.speciality!;
-
-                  setState(() {
-                    horse = horse;
-                    controllers = controllers;
-                  });
-                },
-              ),
-              IconButton(
-                onPressed: () {
-                  addHorse();
-                },
-                icon: const Icon(Icons.add),
-              ),
-              TextField(
-                controller: controllers[0],
-                decoration: const InputDecoration(hintText: 'Nom'),
-                onChanged: (value) {},
-              ),
-              TextField(
-                controller: controllers[1],
-                decoration: const InputDecoration(hintText: 'Photo'),
-                onChanged: (value) {},
-              ),
-              TextField(
-                controller: controllers[2],
-                decoration: const InputDecoration(hintText: 'Âge'),
-                onChanged: (value) {},
-              ),
-              TextField(
-                controller: controllers[3],
-                decoration: const InputDecoration(hintText: 'Robe'),
-                onChanged: (value) {},
-              ),
-              TextField(
-                controller: controllers[4],
-                decoration: const InputDecoration(hintText: 'Race'),
-                onChanged: (value) {},
-              ),
-              TextField(
-                controller: controllers[5],
-                decoration: const InputDecoration(hintText: 'Genre'),
-                onChanged: (value) {},
-              ),
-              TextField(
-                controller: controllers[6],
-                decoration: const InputDecoration(hintText: 'Spécialité'),
-                onChanged: (value) {},
-              ),
-              TextButton(
-                  onPressed: () {
-                    updateHorse();
-                  },
-                  child: const Text('Mettre à jour le cheval')),
-              TextButton(
-                  onPressed: () {
-                    deleteHorse();
-                  },
-                  child: const Text('Supprimer le cheval')),
-            ],
-          ),
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                })
+                          },
+                        ),
+                      ],
+                    ))
+                ]));
+              } else {
+                return const Center(child: Text("No data"));
+              }
+            }
+          },
         )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _showDialog();
+          },
+          backgroundColor: Colors.blue,
+          child: const Icon(Icons.add,
+              color: Colors.white, size: 30.0, semanticLabel: 'Add'),
+        ),
         bottomNavigationBar: const NavBar());
   }
 
-  getHorses() async {
-    List horses = await MongoDatabase.getHorses();
+  Future<void> _showDialog() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Ajouter un cheval'),
+            content: SingleChildScrollView(
+                child: Column(
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    name = value;
+                  },
+                  decoration:
+                      const InputDecoration(labelText: 'Nom', hintText: 'Nom'),
+                ),
+                TextField(
+                  onChanged: (value) {
+                    photo = value;
+                  },
+                  decoration: const InputDecoration(
+                      labelText: 'Photo', hintText: 'Photo'),
+                ),
+                TextField(
+                  onChanged: (value) {
+                    age = value;
+                  },
+                  decoration:
+                      const InputDecoration(labelText: 'Age', hintText: 'Age'),
+                ),
+                TextField(
+                  onChanged: (value) {
+                    dress = value;
+                  },
+                  decoration: const InputDecoration(
+                      labelText: 'Robe', hintText: 'Robe'),
+                ),
+                TextField(
+                    onChanged: (value) {
+                      race = value;
+                    },
+                    decoration: const InputDecoration(
+                        labelText: 'Race', hintText: 'Race')),
+                TextField(
+                    onChanged: (value) {
+                      gender = value;
+                    },
+                    decoration: const InputDecoration(
+                        labelText: 'Gender', hintText: 'Gender')),
+                TextField(
+                    onChanged: (value) {
+                      speciality = value;
+                    },
+                    decoration: const InputDecoration(
+                        labelText: 'Race', hintText: 'Race')),
+              ],
+            )),
+            actions: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                child: const Text('Cancel'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                child: const Text('Confirmer'),
+                onPressed: () async {
+                  var _id = M.ObjectId();
+                  if(name == "" || photo == "" || age == "" || dress == "" || race == ""|| gender == ""|| speciality == ""){
+                    return;
+                  }else{
+                    var query =
+                    await MongoDatabase.db.collection("horses").insertOne({
+                      "_id": _id,
+                      "_ownerId" : MongoDatabase.idUser,
+                      "name": name,
+                      "photo": photo,
+                      "age": age,
+                      "dress": dress,
+                      "race": race,
+                      "gender": gender,
+                      "speciality": speciality,
+                    }).whenComplete(() {
+                      Navigator.of(context).pop();
+                    });
+                    setState(() {
+                      name = "";
+                      photo = "";
+                      age = "";
+                      dress = "";
+                      race = "";
+                      gender = "";
+                      speciality = "";
+                    });
+                  }
 
-    if (horses.isEmpty) {
-      return;
-    }
-
-    for (int i = 0; i < horses.length; i++) {
-      var currHorse = horses[i];
-      var id = currHorse['_id'].$oid;
-      var name = currHorse['name'];
-      var photo = currHorse['photo'];
-      var age = currHorse['age'];
-      var dress = currHorse['dress'];
-      var race = currHorse['race'];
-      var gender = currHorse['gender'];
-      var speciality = currHorse['speciality'];
-
-      HorseModel horse =
-          HorseModel(name, photo, age, dress, race, gender, speciality);
-
-      horse.id = id;
-
-      _horses.add(horse);
-      _horsesNames.add(name);
-    }
-
-    setState(() {
-      _horses = _horses;
-      _horsesNames = _horsesNames;
-
-      if (_horses.isNotEmpty) {
-        horse = _horses.first;
-      }
-    });
+                },
+              ),
+            ],
+          );
+        });
   }
 
-  addHorse() async {
-    HorseModel horse =
-        HorseModel('Horse${Random().nextInt(10000)}', '', '', '', '', '', '');
-
-    horse.id = await MongoDatabase.addHorse(horse);
-    _horses.add(horse);
-
-    setState(() {
-      _horses = _horses;
-      _horsesNames.add(horse.name!);
-    });
-  }
-
-  updateHorse() async {
-    await MongoDatabase.updateHorse(
-        horse.name,
-        HorseModel(
-            controllers[0].text,
-            controllers[1].text,
-            controllers[2].text,
-            controllers[3].text,
-            controllers[4].text,
-            controllers[5].text,
-            controllers[6].text));
-  }
-
-  deleteHorse() async {
-    await MongoDatabase.deleteHorse(horse.name);
-  }
+  //
+  // addHorse() async {
+  //   var _id = M.ObjectId();
+  //   HorseModel horse = HorseModel(
+  //       _id, 'Horse${Random().nextInt(10000)}', '', '', '', '', '', '');
+  //
+  //   horse.id = await MongoDatabase.addHorse(horse);
+  //   _horses.add(horse);
+  //
+  //   setState(() {
+  //     _horses = _horses;
+  //     _horsesNames.add(horse.name!);
+  //   });
+  // }
+  //
+  // deleteHorse() async {
+  //   await MongoDatabase.deleteHorse(horse.name);
+  // }
 }
